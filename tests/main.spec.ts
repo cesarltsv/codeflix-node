1851177d773ed0fccbff7429d44eaa8a98c9c85d
cezarltsv <cezarltsv@gmail.com>
2025-08-01 22:40:32 -0300

    fix: eslint

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,5 +1,5 @@
-import { HelloFuntion } from "../src/main";
-it("should behave...", () => {
-  const result = HelloFuntion();
-  expect(result).toBe(6);
-});
+import { HelloFuntion } from '../src/main'
+it('should behave...', () => {
+    const result = HelloFuntion()
+    expect(result).toBe(6)
+})
